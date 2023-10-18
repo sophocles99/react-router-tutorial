28089eea4c13e33f815a9a2d2466b4e5cce08a93
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-10-18 15:58:41 +0100

    use useSubmit to submit search form on every keypress

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,6 +7,7 @@ import {
   redirect,
   useLoaderData,
   useNavigation,
+  useSubmit,
 } from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
 
@@ -25,6 +26,7 @@ export async function loader({ request }) {
 export default function Root() {
   const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+  const submit = useSubmit();
 
   useEffect(() => {
     document.getElementById("q").value = q;
@@ -43,6 +45,9 @@ export default function Root() {
               type="search"
               name="q"
               defaultValue={q}
+              onChange={(event) => {
+                submit(event.currentTarget.form);
+              }}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
